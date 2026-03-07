866622b6d8c0172c5a18bccaca5503e2cd924e64
Frank Chan <thefrankchan@gmail.com>
2026-03-07 17:57:08 +0000

    chore: update mdcat to 0.1.8

--- Formula/mdcat.rb
+++ Formula/mdcat.rb
@@ -4,10 +4,10 @@
 class Mdcat < Formula
   desc "Terminal pager for Markdown with syntax highlighting and search"
   homepage "https://github.com/dunkinfrunkin/mdcat"
-  url "https://registry.npmjs.org/@dunkinfrunkin/mdcat/-/mdcat-0.1.7.tgz"
-  sha256 "64c33c862de692405dbe5c32fa862772aa7a2f5bccbee051f287334847d43ae3"
+  url "https://registry.npmjs.org/@dunkinfrunkin/mdcat/-/mdcat-0.1.8.tgz"
+  sha256 "9d9a47a067569bfa6496371c57ce696b694a43c1d2df4a20d699e976e005c34a"
   license "MIT"
-  version "0.1.7"
+  version "0.1.8"
 
   depends_on "node"
 
