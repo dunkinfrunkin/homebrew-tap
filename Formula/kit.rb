26b9e40a02a18d24a1d2bae25c39fdb89ba69b51
frankchan <thefrankchan@gmail.com>
2026-03-25 12:38:15 -0700

    Update kit to v0.1.1

--- Formula/kit.rb
+++ Formula/kit.rb
@@ -1,28 +1,28 @@
 class Kit < Formula
   desc "Share AI coding skills, hooks, and config across tools and teams"
   homepage "https://github.com/dunkinfrunkin/kit"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/dunkinfrunkin/kit/releases/download/v#{version}/kit_darwin_arm64.tar.gz"
-      sha256 "6a18b9d5395af9f9a9d297ef65d5d7e7413154d3c6ddd72e1af253e2a7bd635c"
+      sha256 "707b477bbc67e87bd85e9ed76014c476288bccef3a059faa5c9cbb233beb1726"
     end
     on_intel do
       url "https://github.com/dunkinfrunkin/kit/releases/download/v#{version}/kit_darwin_amd64.tar.gz"
-      sha256 "a090596f60d426a9dc10469648727def2f149ff70bffd0a5f1fc4f3fb5d04e45"
+      sha256 "234f6d9e5ce9e90d31d34afc94a78b5615275238ab9f6e68a82faa657abe6cba"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/dunkinfrunkin/kit/releases/download/v#{version}/kit_linux_arm64.tar.gz"
-      sha256 "d7e8283acc89a140bf627dcc9dd85f4507e3529691c679720abb2966f3dfff3c"
+      sha256 "6ddb625778e09a9ad38269b7297c4d15dc2affee630e8e1d22e649cda78e7090"
     end
     on_intel do
       url "https://github.com/dunkinfrunkin/kit/releases/download/v#{version}/kit_linux_amd64.tar.gz"
-      sha256 "79da69fed77a431177c2624d28b5406b58e0eb83bf41ede5bf803f7c0e3b9ab9"
+      sha256 "5d3019e80621607efee59724d2cda8d9932eb3661c018b62169f56a903619b84"
     end
   end
 
