9c3a26655447c093f511ee05fd918189675f00df
Frank Chan <thefrankchan@gmail.com>
2026-04-20 19:07:29 +0000

    Update maestro to 0.35.1

--- Formula/maestro.rb
+++ Formula/maestro.rb
@@ -1,28 +1,28 @@
 class Maestro < Formula
   desc "Autonomous coding agent orchestration for engineering teams"
   homepage "https://github.com/dunkinfrunkin/maestro"
-  version "0.35.0"
+  version "0.35.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/dunkinfrunkin/maestro/releases/download/v#{version}/maestro_darwin_arm64.tar.gz"
-        sha256 "0884f96e89e51f79518106812ffe743681a505fcc1781f3ab7924a275b61cfe3"
+        sha256 "2c0bdf32154b1df2ad8d7eae2cb0415a25ece7f3bad8b185294c3dfa5c996c74"
     end
     on_intel do
       url "https://github.com/dunkinfrunkin/maestro/releases/download/v#{version}/maestro_darwin_amd64.tar.gz"
-        sha256 "b4623f37c7abd0c59abd53ffc6bad941eb1e2bf253ecf949fed85ef684874dba"
+        sha256 "7de4f259a9e10fdc9e5a17d5f2a5c3bc6162c6b50f5b7209f8ddc2f5658706a2"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/dunkinfrunkin/maestro/releases/download/v#{version}/maestro_linux_arm64.tar.gz"
-        sha256 "39e34c353e467ae6bbd85080cd1ed7e9b65cb4fe53082577baf4f35bc1881f37"
+        sha256 "aca4bedcb7c041d620e265342c6bd825f620566ceff832048d0acd17177c7e7c"
     end
     on_intel do
       url "https://github.com/dunkinfrunkin/maestro/releases/download/v#{version}/maestro_linux_amd64.tar.gz"
-        sha256 "211401147cd5a642385179b6a5f4305c2f8baaf3eec07fb2ef81c9d3775a0cfd"
+        sha256 "3f24771e6813bc941b476aa9eac473f9ce13b85450bd9ab6df9c8d92a253a217"
     end
   end
 
